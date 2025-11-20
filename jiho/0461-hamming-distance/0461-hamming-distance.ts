3c76c1c555d646684553b07b66852f529a5be5bb
권지호 <xxziiko@gmail.com>
2025-11-20 22:22:53 +0900

    💡 [LTC] Bit Manipulation: 0461-hamming-distance
    - Time: 0 ms (100%)
    - Space: 55.4 MB (45.16%)
    - Difficulty: Easy
    - Date: 11-20-2025

--- jiho/0461-hamming-distance/0461-hamming-distance.ts
+++ jiho/0461-hamming-distance/0461-hamming-distance.ts
@@ -1,3 +1,16 @@
+// function hammingDistance(x: number, y: number): number {
+//     return (x^y).toString(2).split('1').length - 1
+// };
+
 function hammingDistance(x: number, y: number): number {
-    return (x^y).toString(2).split('1').length - 1
+    let z = x^y
+    let count = 0
+    
+
+    while(z > 0) {
+        count += z & 1;
+        z >>= 1; 
+    }
+
+    return count
 };
