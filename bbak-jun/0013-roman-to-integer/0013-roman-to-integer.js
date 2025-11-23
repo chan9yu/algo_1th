43c87d718fd94c11e8d275a8564fd6717adc392b
박준형 <33216504+BBAK-jun@users.noreply.github.com>
2025-11-23 21:24:26 +0900

    Time: 8 ms (23.1%), Space: 61.2 MB (25.4%) - LeetHub

---
+++ bbak-jun/0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function romanToInt(s) {
+    const map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let curr = map[s[i]];
+        let next = map[s[i + 1]];
+
+        // 다음 값이 더 크면 → 빼기 (IV, IX, XL, XC, CD, CM)
+        if (next > curr) {
+            result -= curr;
+        } else {
+            result += curr;
+        }
+    }
+
+    return result;
+};
